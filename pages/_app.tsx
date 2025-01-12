e4f76d4eb96a5fd2b471d538907c37039267abad
hyfetch <128172119+hyfetch@users.noreply.github.com>
2025-01-12 18:47:41 +0100

    This, is the sole reason why i do hate next.js. After adding localization i can't seem to figure out where this goes.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import '../styles/global.css';
 import { AppProps } from 'next/app'; 
 import LocalizationProvider from './LocalizationContext'; 
+import Head from 'next/head';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LocalizationProvider>
+      <Head>
+      <meta name="description" content="a silly site :3" />
+      <meta name="author" content="mstrv" />
+      <meta property="og:title" content="mstrv - portfolio" />
+      <meta property="og:description" content="a silly site :3" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://mero.gay" />
+      <meta property="og:image" content="https://contact.mero.lol/images/peek.png" />
+      <meta name="twitter:title" content="mstrv - portfolio" />
+      <meta name="twitter:description" content="a silly site :3" />
+      <meta name="twitter:image" content="https://contact.mero.lol/images/peek.png" />
+      </Head>
       <Component {...pageProps} />
     </LocalizationProvider>
   );
 }
-
-export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,6 @@ const Home = () => (
   <div className="bg-gray-100 min-h-screen">
     <Head>
       <title>mstrv - portfolio</title>
-      <meta name="description" content="a silly site :3" />
-      <meta name="author" content="mstrv" />
-      <meta property="og:title" content="mstrv - portfolio" />
-      <meta property="og:description" content="a silly site :3" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://mero.gay" />
-      <meta property="og:image" content="https://contact.mero.lol/images/peek.png" />
-      <meta name="twitter:title" content="mstrv - portfolio" />
-      <meta name="twitter:description" content="a silly site :3" />
-      <meta name="twitter:image" content="https://contact.mero.lol/images/peek.png" />
     </Head>
     <div className="wrapper">
       <div className="content max-w-4xl mx-auto py-8 px-4">
