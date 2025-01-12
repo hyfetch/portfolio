0c4e0294f7063e68408b9950e8ae7f2c6434ca6e
hyfetch <128172119+hyfetch@users.noreply.github.com>
2025-01-12 18:53:26 +0100

    hotfix v2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,24 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LocalizationProvider>
       <Head>
-      <meta name="description" content="a silly site :3" />
-      <meta name="author" content="mstrv" />
-      <meta property="og:title" content="mstrv - portfolio" />
-      <meta property="og:description" content="a silly site :3" />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://mero.gay" />
-      <meta property="og:image" content="https://contact.mero.lol/images/peek.png" />
-      <meta name="twitter:title" content="mstrv - portfolio" />
-      <meta name="twitter:description" content="a silly site :3" />
-      <meta name="twitter:image" content="https://contact.mero.lol/images/peek.png" />
+      <Head>
+  <meta name="description" content="a silly site :3" />
+  <meta name="author" content="mstrv" />
+  
+    <meta property="og:title" content="mstrv - portfolio" />
+    <meta property="og:description" content="a silly site :3" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://mero.gay" />
+    <meta property="og:image" content="https://contact.mero.lol/images/peek.png" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:site_name" content="mstrv portfolio" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="mstrv - portfolio" />
+    <meta name="twitter:description" content="a silly site :3" />
+    <meta name="twitter:image" content="https://contact.mero.lol/images/peek.png" />
+    {/*This is why this repo was private for so long, because i did small commits to experiment ;p */}
+</Head>
       </Head>
       <Component {...pageProps} />
     </LocalizationProvider>
